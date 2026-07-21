0fb03139fe7d0dd1ae4d787b30de1d08d9aee03f
carlamonaliza9-rgb <carlamonaliza9@gmail.com>
2026-07-21 10:49:31 -0300

    tentando

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,28 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
 };
 
-declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("A variável DATABASE_URL não foi configurada.");
 }
 
-const prisma = globalThis.prisma ?? prismaClientSingleton();
+const adapter = new PrismaPg({
+  connectionString,
+});
 
-export { prisma };
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
